86bf7052eaded03880bd439e9b6fc8e1fea13aa7
Drogonito <drogonito@pop-os.localdomain>
2024-06-05 18:26:49 +0200

    optimal_path.py : find path of snowed roads with multiples vehicule
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

--- src/truck_path/optimal_path.py
+++ src/truck_path/optimal_path.py
@@ -1,18 +1,18 @@
 import networkx as nx
 
 def find_paths_for_edges(G, edges, start_node):
-    if not edges:
-        return []
-
     path_edges = []
     current_node = start_node
 
     for edge in edges:
-        if current_node != edge[0]:
-            path_to_next_edge = nx.shortest_path(G, current_node, edge[0])
-            path_edges.extend(zip(path_to_next_edge[:-1], path_to_next_edge[1:]))
-        path_edges.append(edge)
-        current_node = edge[1]
+        try:
+            if current_node != edge[0]:
+                path_to_next_edge = nx.shortest_path(G, current_node, edge[0])
+                path_edges.extend(zip(path_to_next_edge[:-1], path_to_next_edge[1:]))
+            path_edges.append(edge)
+            current_node = edge[1]
+        except nx.NetworkXNoPath:
+            continue
 
     return path_edges
 
@@ -31,7 +31,7 @@ def test():
     G = nx.Graph()
     G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (5, 1), (1, 3), (2, 4), (3, 5), (4, 1)])
     edges_of_interest = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1), (1, 3), (2, 4)]
-    number_of_vehicule = 1
+    number_of_vehicule = 3
     starting_node = 2
     routes = assign_edges_to_vehicles(G, edges_of_interest, number_of_vehicule, starting_node)
     print(routes)
