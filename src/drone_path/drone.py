bd814ec17a5e852d351a7dc3b50e715c1e157d62
frederic.dong <frederic.dong@epita.fr>
2024-06-06 17:11:07 +0200

    [feat] Final version wiating to be approved

--- src/drone_path/drone.py
+++ src/drone_path/drone.py
@@ -1,70 +1,10 @@
 import networkx as nx
-import numpy as np
-from scipy.optimize import linear_sum_assignment
-import matplotlib.pyplot as plt
-import sys 
 from map import get_district_graph
-from networkx.drawing.nx_pydot import write_dot
+from eulerize import eulerize_undirected
 import threading
-from itertools import combinations
-
-from joblib import Parallel, delayed
-import multiprocessing
-
-import networkx as nx
-from itertools import combinations
-from functools import lru_cache
-
-def eulerize(G):
-    if G.order() == 0:
-        raise nx.NetworkXPointlessConcept("Cannot Eulerize null graph")
-    if not nx.is_connected(G):
-        raise nx.NetworkXError("G is not connected")
-
-    odd_degree_nodes = [n for n, d in G.degree() if d % 2 == 1]
-    G = nx.MultiGraph(G)
-    
-    if len(odd_degree_nodes) == 0:
-        return G
-
-    # Use Johnson's algorithm for all-pairs shortest paths for sparse graphs
-    all_pairs_shortest_paths = dict(nx.johnson(G))
-
-    # Create a list of pairs (m, n) with their shortest path lengths
-    odd_deg_pairs = [
-        (m, n, len(all_pairs_shortest_paths[m][n]))
-        for m, n in combinations(odd_degree_nodes, 2)
-    ]
-    
-    # Sort pairs by path length
-    odd_deg_pairs.sort(key=lambda x: x[2])
-
-    # Use a greedy algorithm to find a matching
-    matched = set()
-    matching = []
-    for m, n, length in odd_deg_pairs:
-        if m not in matched and n not in matched:
-            matched.add(m)
-            matched.add(n)
-            matching.append((m, n))
-
-    # Add the paths to the graph
-    for m, n in matching:
-        path = all_pairs_shortest_paths[m][n]
-        G.add_edges_from(nx.utils.pairwise(path))
-
-    return G
-
-def eurilize_graph(graph):
-    return eulerize(graph)
-
-# à faire
-
-def find_len(graph, path,log=False):
+def find_len(graph, path):
     res = 0
     for (u,v) in path:
-        if (log):
-            print(f"u is {u} and v is {v} weight is {graph[u][v].get('weight', 1)}")
         res += graph[u][v].get('weight', 1)
     return res
 
@@ -80,35 +20,43 @@ def find_drone_path(district, log=False):
     trouver le chemin eulerian
     retourner chemin
     """
-    # print(f"Charging {district}")
+    if (log):
+        print(f"Charging {district}")
     graph = get_district_graph(district).to_undirected()
-    # print(f"Finished Charging {district}")
+    if (log):
+        print(f"Finished Charging {district}")
     if (not nx.is_eulerian(graph)):
-        # print(f"Eularizing {district}")
-        result = eurilize_graph(graph)
-        # print(f"Finished Eularizing {district}")
-    # print(f"Searching the eulerian circuit of {district}")
+        if (log):
+            print(f"Eularizing {district}")
+        result = eulerize_undirected(graph,log=log)
+        if (log):
+            print(f"Finished Eularizing {district}")
+    if (log):
+        print(f"Searching the eulerian circuit of {district}")
     circuit = nx.algorithms.euler.eulerian_circuit(result)
-    # print(f"Finished the eulerian circuit of {district}")
-    # print(f"Calculating len {district}")
     res= list(circuit)
-    print(f"len for {district} is {find_len(graph,res)}")
+    if (log):
+        print(f"Finished the search of the eulerian circuit of {district}")
+        len = find_len(graph,res)
+        print(f"The len is {len}")
     return res
 
-t1 = threading.Thread(target=find_drone_path("Outremont"))
-t2 = threading.Thread(target=find_drone_path("Verdun"))
-t3 = threading.Thread(target=find_drone_path("Anjou"))
-t4 = threading.Thread(target=find_drone_path("Rivière-des-prairies-pointe-aux-trembles"))
-t5 = threading.Thread(target=find_drone_path("Le Plateau-Mont-Royal"))
+def test():
+    
+    d1 = threading.Thread(target=find_drone_path, args=("Outremont",), kwargs={"log": True})
+    d2 = threading.Thread(target=find_drone_path, args=("Verdun",), kwargs={"log": True})
+    d3 = threading.Thread(target=find_drone_path, args=("Anjou",), kwargs={"log": True})
+    d4 = threading.Thread(target=find_drone_path, args=("Rivière-des-prairies-pointe-aux-trembles",), kwargs={"log": True})
+    d5 = threading.Thread(target=find_drone_path, args=("Le Plateau-Mont-Royal",), kwargs={"log": True})
 
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
+    d1.start()
+    d2.start()
+    d3.start()
+    d4.start()
+    d5.start()
 
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
+    d1.join()
+    d2.join()
+    d3.join()
+    d4.join()
+    d5.join()
--- src/drone_path/eulerize.py
+++ src/drone_path/eulerize.py
@@ -1,67 +1,43 @@
 import networkx as nx
-from scipy.optimize import linear_sum_assignment
+from itertools import combinations
 
-# à faire
-def eurilize_graph(graph):
+def eulerize_undirected(G, log = False):
     """
-    Si nombre de noeud a degré impair est impair
-        ajouter un noeud de degré impair
-    Si les edges ne sont pas connecté
-        les connecter
-    retourner le graph
+    transformer le graphe non orienté en un graph orienté eulerian
     """
-    # Step 1: Identify vertices of odd degree
-    odd_degree_nodes = [v for v, d in graph.degree() if d % 2 != 0]
-    print(f"Odd degree nodes: {odd_degree_nodes}")
+    if G.order() == 0:
+        print("G.order est 0")
+        exit(2)
+    if not nx.is_connected(G):
+        print("G est pas connecté")
+        exit(2)
+    nodes_ = [n for n, d in G.degree() if d % 2 == 1]
+    G = nx.MultiGraph(G)
     
-    # Step 2: Create a complete graph of odd degree vertices with edge weights as shortest path distances
-    subgraph = nx.Graph()
-    for i, node1 in enumerate(odd_degree_nodes):
-        for j, node2 in enumerate(odd_degree_nodes):
-            if i < j:
-                distance = nx.shortest_path_length(graph, node1, node2, weight='weight')
-                subgraph.add_edge(node1, node2, weight=distance)
+    if len(nodes_) == 0:
+        return G
+    if (log):
+        print("Johnson algorithm")
+
+    pairs_ = dict(nx.johnson(G))
+    if (log):
+        print("Creating list of shortests path")
+    odd_ = [ (m, n, len(pairs_[m][n])) for m, n in combinations(nodes_, 2)]
     
-    # Step 3: Use the Hungarian algorithm to find the minimum weight matching
-    weight_matrix = nx.to_numpy_matrix(subgraph, weight='weight')
-    row_ind, col_ind = linear_sum_assignment(weight_matrix)
-    min_weight_matching = [(odd_degree_nodes[i], odd_degree_nodes[j]) for i, j in zip(row_ind, col_ind)]
-    print(f"Minimum weight matching: {min_weight_matching}")
+    odd_.sort(key=lambda x: x[2])
+    if (log):
+        print("Matching algorithm")
+    set_ = set()
+    list_path_ = []
+    for m, n, length in odd_:
+        if m not in set_ and n not in set_:
+            set_.add(m)
+            set_.add(n)
+            list_path_.append((m, n))
+    if (log):
+        print("Adding the path")
+    for m, n in list_path_:
+        path = pairs_[m][n]
+        G.add_edges_from(nx.utils.pairwise(path))
 
-    # Step 4: Add the edges from the minimum weight matching to the original graph
-    for u, v in min_weight_matching:
-        path = nx.shortest_path(graph, u, v, weight='weight')
-        for i in range(len(path) - 1):
-            graph.add_edge(path[i], path[i + 1], weight=graph[path[i]][path[i + 1]].get('weight', 1))
-        
-    if not nx.is_eulerian(graph):
-        print("HMAR DINMOK")
-        exit()
-
-    return graph
-
-# à faire
-def find_eulerian_path(district, log=False):
-    """
-    Si graphe n est pas eulerian
-        transformer le graphe en eulerian avec eulerize_graph
-    sinon:
-        chemin[]
-        pile avec n'importe quel sommet de départ
-        tant que chemin non visité:
-            si sommet a des arrêtes non visité
-                empiler le sommet sur la pile
-                choisir un sommet non visité
-                marquer l'arrête comme visité
-                deplacer vers un voisin pas visité
-            sinon:
-                ajouter le sommet dans chemin 
-                dépiler
-    retourner le chemin
-    """
-    if log:
-        print("Recherche de chemin en cours")
-    path = nx.algorithms.euler.eulerian_circuit(eulerized_graph)
-    if log:
-        print("Chemin trouvé")
-    return list(path)
+    return G
